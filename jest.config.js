52dbbc56ca298c03977ac8d86296dc9186e05978
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-08-28 22:18:34 -0300

    refactor: move duplicated code to helper

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   clearMocks: true,
-  // collectCoverage: true,
-  // collectCoverageFrom: [
-    // '<rootDir>/src/**/*.ts'
-  // ],
-  // coverageDirectory: "coverage",
-  // coverageProvider: "v8",
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
   transform: {
