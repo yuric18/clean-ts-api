a082e9295728f9a1d554bb13cd56eabd4cc5d151
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-08-31 17:47:46 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/EmailValidatorAdapter.spec.ts
+++ src/utils/EmailValidatorAdapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from "./EmailValidator";
+import validator from 'validator';
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true;
+  }
+}));
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const result = sut.isValid('invalid_email@mail.com');
     expect(result).toBeFalsy();
+  });
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter();
+    const result = sut.isValid('valid_email@mail.com');
+    expect(result).toBeTruthy();
   });
 });
