44ab2f0ca99f153a03267b2aebf4f4347e73157b
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-08-31 17:14:21 -0300

    refactor: change import

--- src/presentation/helpers/HttpHelper.ts
+++ src/presentation/helpers/HttpHelper.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from "../protocols/IHttp";
+import { HttpResponse } from "../protocols";
 import { ServerError } from "../errors";
 
 export const badRequest = (error: Error): HttpResponse => ({
