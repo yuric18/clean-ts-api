e129b576d83210b29e3caf16771afb0059a3fab3
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-08-31 22:24:15 -0300

    feat: ensures BcryptAdapter returns a hash on success

--- src/infra/criptography/BcryptAdapter.ts
+++ src/infra/criptography/BcryptAdapter.ts
@@ -7,8 +7,7 @@ export class BcryptAdapter implements Encrypter {
   ) {}
 
   async encrypt(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return null;
+    return await bcrypt.hash(value, this.salt);
   }
 
 };
