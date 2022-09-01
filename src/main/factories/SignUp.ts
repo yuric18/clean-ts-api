127449fd2ee3a84e46819f519321bb08f9b912b9
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-01 02:47:44 -0300

    feat: add signup factory

---
+++ src/main/factories/SignUp.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../presentation/controllers/signup/SignUp';
+import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter';
+import { DbAddAccount } from '../../data/usecases/addAccount/DbAddAccount';
+import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter';
+import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/Account";
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12;
+  const emailValidatorAdapter = new EmailValidatorAdapter();
+
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const accountMongoRepository = new AccountMongoRepository();
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+
+  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+}
