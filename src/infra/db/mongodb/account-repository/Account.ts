6ec867879bdd055bb084df3a67d4fb8eb73d8542
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-01 01:30:30 -0300

    feat: ensures AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/Account.ts
@@ -0,0 +1,13 @@
+import {
+  AccountModel,
+  AddAccountModel,
+  AddAccountRepository
+} from '../../../../data/usecases/addAccount/DbAddAccountProtocols';
+import { MongoHelper } from '../helpers/MongoHelper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    MongoHelper.getCollection('accounts');
+    return await MongoHelper.insert<AddAccountModel>(accountData);
+  }
+}
--- src/infra/db/mongodb/helpers/MongoHelper.ts
+++ src/infra/db/mongodb/helpers/MongoHelper.ts
@@ -1,11 +1,24 @@
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient, ObjectId } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
+  collection: null as Collection,
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri);
   },
   async disconnect(): Promise<void> {
     await this.client.close();
+  },
+  getCollection(name: string): void {
+    this.collection = this.client.db().collection(name);
+  },
+  async insert<T>(data: T): Promise<T & { id: string }> {
+    const options = { upsert: true, returnDocument: 'after' };
+    const { value: document } = await this.collection.findOneAndUpdate(
+      { _id: new ObjectId() },
+      { $setOnInsert: data },
+      options
+    );
+    return { ...document, id: document._id };
   }
 };
