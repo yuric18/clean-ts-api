9c93a4cf921db04ee85cc27f60186bb049513e3d
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-04 21:55:39 -0300

    refactor: changes token variable name to accessToken

--- src/data/usecases/authentication/DbAuthentication.spec.ts
+++ src/data/usecases/authentication/DbAuthentication.spec.ts
@@ -58,7 +58,7 @@ describe('DbAuthentication UseCase', () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut();
     jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
       .mockReturnValueOnce(null);
-    const token = await sut.auth(makeFakeAuthentication());
-    expect(token).toBeNull();
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBeNull();
   });
 });
