9c13984e9756070b94efd120c58afd11b0c2457e
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-06 03:27:22 -0300

    test: updates test suite name

--- src/main/routes/LoginRoutes.test.ts
+++ src/main/routes/LoginRoutes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/MongoHelper";
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
 
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
@@ -17,15 +17,17 @@ describe('SignUp Routes', () => {
     await MongoHelper.collection.deleteMany({});
   });
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Yuri',
-        email: 'y.cabral18@hotmail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Yuri',
+          email: 'y.cabral18@hotmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200);
+    })
   })
 })
