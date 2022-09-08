8e2a83e70e4b9427379c7aa3ebf7bb8a751d75ee
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-08 04:07:03 -0300

    fix: adds authentication to signupcontroller factory

--- src/main/factories/signup/SignUpFactory.ts
+++ src/main/factories/signup/SignUpFactory.ts
@@ -7,14 +7,24 @@ import { Controller } from '../../../presentation/protocols';
 
 import { LogControllerDecorator } from '../../decorators/LogControllerDecorator';
 import { makeSignUpValidation } from './SignUpValidationFactory';
+import {JwtAdapter} from "../../../infra/criptography/jwt/JwtAdapter";
+import env from "../../config/env";
+import {DbAuthentication} from "../../../data/usecases/authentication/DbAuthentication";
 
 
 export const makeSignUpController = (): Controller => {
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
   const accountMongoRepository = new AccountMongoRepository();
+  const jwtAdapter = new JwtAdapter(env.jwtSecret);
+  const authentication = new DbAuthentication(
+    accountMongoRepository,
+    bcryptAdapter,
+    jwtAdapter,
+    accountMongoRepository,
+  );
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
   const logMongoRepository = new LogMongoRepository();
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation());
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), authentication);
   return new LogControllerDecorator(signUpController, logMongoRepository);
 };
