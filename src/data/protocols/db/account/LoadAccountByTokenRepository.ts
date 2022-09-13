86a0106cb98ae5eabb525e5120c2d8721d503bff
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-13 02:15:54 -0300

    feat: adds role to LoadAccountByTokenRepository

--- src/data/protocols/db/account/LoadAccountByTokenRepository.ts
+++ src/data/protocols/db/account/LoadAccountByTokenRepository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../../usecases/addAccount/DbAddAccountProtocols';
 
 export interface LoadAccountByTokenRepository {
-  loadByToken(token: string): Promise<AccountModel>;
+  loadByToken(token: string, role?: string): Promise<AccountModel>;
 }
