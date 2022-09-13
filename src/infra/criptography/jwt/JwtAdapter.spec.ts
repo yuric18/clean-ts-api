ac211763cfb17694aec60c43f7571dc12aa16eec
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-13 03:50:01 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt/JwtAdapter.spec.ts
+++ src/infra/criptography/jwt/JwtAdapter.spec.ts
@@ -58,5 +58,13 @@ describe('JWT Adapter', () => {
       const value = await sut.decrypt('any_token');
       expect(value).toBe('any_value');
     });
+
+    test('Should throw if verify throws', () => {
+      const { sut } = makeSut();
+      jest.spyOn(jwt, 'verify')
+        .mockImplementationOnce(() => { throw new Error(); });
+      const promise = sut.decrypt('any_id');
+      expect(promise).rejects.toThrow();
+    });
   });
 });
