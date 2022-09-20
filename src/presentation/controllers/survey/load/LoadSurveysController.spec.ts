c54f7b1397994bf0d31cdf1cfd898d66a23136f5
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-09-20 03:47:40 -0300

    feat: ensures LoadSurveysController return 200 on success

--- src/presentation/controllers/survey/load/LoadSurveysController.spec.ts
+++ src/presentation/controllers/survey/load/LoadSurveysController.spec.ts
@@ -1,6 +1,7 @@
 import MockDate from 'mockdate';
 import { LoadSurveysController } from './LoadSurveysController';
 import { LoadSurveys, SurveyModel } from './LoadSurveysControllerProtocols';
+import { ok } from '../../../helpers/http/HttpHelper';
 
 const makeFakeSurveys = (): SurveyModel[] => ([{
   question: 'any_question',
@@ -52,5 +53,11 @@ describe('Load Surveys Controller', () => {
     const loadSpy = jest.spyOn(loadSurveysStub, 'load');
     await sut.handle({});
     expect(loadSpy).toHaveBeenCalled();
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()));
   });
 });
--- src/presentation/controllers/survey/load/LoadSurveysController.ts
+++ src/presentation/controllers/survey/load/LoadSurveysController.ts
@@ -1,3 +1,4 @@
+import { ok } from '../../../helpers/http/HttpHelper';
 import { Controller, HttpResponse, HttpRequest, LoadSurveys } from './LoadSurveysControllerProtocols';
 
 export class LoadSurveysController implements Controller {
@@ -6,8 +7,8 @@ export class LoadSurveysController implements Controller {
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load();
-    return null;
+    const surveys = await this.loadSurveys.load();
+    return ok(surveys);
   }
 
 }
