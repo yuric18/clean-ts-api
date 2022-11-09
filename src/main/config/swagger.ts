21aa4d2b1d7021f6416c0fe82dd5093e7b9c1ab9
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-11-09 03:10:54 -0300

    feat: removes cache from swagger route

--- src/main/config/swagger.ts
+++ src/main/config/swagger.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 import config from '@/main/docs';
+import { noCache } from '../middlewares';
 
 export default (app: Express): void => {
-  app.use('/docs/swagger', serve, setup(config));
+  app.use('/docs/swagger', noCache, serve, setup(config));
 };
