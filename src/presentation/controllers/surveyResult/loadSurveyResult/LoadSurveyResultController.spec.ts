5b527a71ff9f0d784aa8c744e2da0ccfe98191c8
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-11-12 07:22:18 -0300

    feat: ensures LoadSurveyResultController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/surveyResult/loadSurveyResult/LoadSurveyResultController.spec.ts
+++ src/presentation/controllers/surveyResult/loadSurveyResult/LoadSurveyResultController.spec.ts
@@ -1,6 +1,10 @@
 import { LoadSurveyResult } from '@/domain/usecases/surveyResult/LoadSurveyResult';
 import { mockLoadSurveyResult } from '@/presentation/test';
-import { HttpRequest } from '../saveSurveyResult/SaveSurveyResultControllerProtocols';
+import {
+  HttpRequest,
+  InvalidParamError,
+} from '../saveSurveyResult/SaveSurveyResultControllerProtocols';
+import { forbidden } from '@/presentation/helpers/http/HttpHelper';
 import { LoadSurveyResultController } from './LoadSurveyResultController';
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -29,5 +33,12 @@ describe('Load Survey Controller', () => {
     const loadSpy = jest.spyOn(loadSurveyResultStub, 'load');
     await sut.handle(makeFakeRequest());
     expect(loadSpy).toHaveBeenCalledWith('any_id');
+  });
+
+  test('should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyResultStub } = makeSut();
+    jest.spyOn(loadSurveyResultStub, 'load').mockResolvedValue(null);
+    const response = await sut.handle(makeFakeRequest());
+    expect(response).toEqual(forbidden(new InvalidParamError('surveyId')));
   });
 });
--- src/presentation/controllers/surveyResult/loadSurveyResult/LoadSurveyResultController.ts
+++ src/presentation/controllers/surveyResult/loadSurveyResult/LoadSurveyResultController.ts
@@ -1,9 +1,11 @@
 import { LoadSurveyResult } from '@/domain/usecases/surveyResult/LoadSurveyResult';
+import { forbidden } from '@/presentation/helpers/http/HttpHelper';
 import {
   Controller,
   HttpRequest,
   HttpResponse,
 } from '@/presentation/protocols';
+import { InvalidParamError } from '../saveSurveyResult/SaveSurveyResultControllerProtocols';
 
 export class LoadSurveyResultController implements Controller {
   constructor(private readonly loadSurveyResult: LoadSurveyResult) {}
@@ -11,7 +13,9 @@ export class LoadSurveyResultController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { surveyId } = httpRequest.params;
 
-    await this.loadSurveyResult.load(surveyId);
+    const surveyResult = await this.loadSurveyResult.load(surveyId);
+
+    if (!surveyResult) return forbidden(new InvalidParamError('surveyId'));
     return null;
   }
 }
