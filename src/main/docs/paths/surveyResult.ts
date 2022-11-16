70bedfb12e2f1d7f424ee60a2ccdd95b98afcf29
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-11-16 01:01:38 -0300

    docs: add load survey result path

--- src/main/docs/paths/surveyResult.ts
+++ src/main/docs/paths/surveyResult.ts
@@ -49,4 +49,45 @@ export const surveyResultPath = {
       },
     },
   },
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+        description: 'Id de uma enquete já criada anteriormente',
+      },
+    ],
+    tags: ['Enquete'],
+    summary: 'API para obter o resultado de uma enquete',
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
 };
