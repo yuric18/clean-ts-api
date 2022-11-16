f6013f269178bc0ac2e6cc0c9ad2129f2856bc7f
Yuri Cabral <yuri.cabral.3@globalhitss.com.br>
2022-11-16 00:50:25 -0300

    feat: ensure LoadSurveyRoute returns 403 if no access token is provided

--- src/main/routes/SurveyResultRoutes.test.ts
+++ src/main/routes/SurveyResultRoutes.test.ts
@@ -75,4 +75,34 @@ describe('Survey Result Routes', () => {
         .expect(200);
     });
   });
+
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 on load survey result without accessToken', async () => {
+      await request(app).get('/api/surveys/any_id/results').expect(403);
+    });
+
+    // test('Should return 200 on save survey result with an valid accessToken', async () => {
+    //   await MongoHelper.getCollection('surveys');
+    //   const data = await MongoHelper.insert({
+    //     question: 'Question',
+    //     answers: [
+    //       {
+    //         answer: 'Answer 1',
+    //         image: 'http://image-name.com',
+    //       },
+    //     ],
+    //     date: new Date(),
+    //   });
+    //   const survey = MongoHelper.map(data);
+
+    //   const accessToken = await makeAccessToken();
+    //   await request(app)
+    //     .put(`/api/surveys/${survey.id}/results`)
+    //     .set('x-access-token', accessToken)
+    //     .send({
+    //       answer: 'Answer 1',
+    //     })
+    //     .expect(200);
+    // });
+  });
 });
--- src/main/routes/SurveyResultRoutes.ts
+++ src/main/routes/SurveyResultRoutes.ts
@@ -1,12 +1,19 @@
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/surveyResult/SaveSurveyResultControllerFactory';
 import { Router } from 'express';
-import { adminAuth } from '../middlewares';
+import { auth } from '../middlewares';
 import { ExpressRouteAdapter } from '@/main/adapters/ExpressRouteAdapter';
+import { makeLoadSurveyResultController } from '../factories/controllers/surveyResult/LoadSurveyResultControllerFactory';
 
 export default (router: Router): void => {
   router.put(
     '/surveys/:surveyId/results',
-    adminAuth,
+    auth,
     ExpressRouteAdapter(makeSaveSurveyResultController())
+  );
+
+  router.get(
+    '/surveys/:surveyId/results',
+    auth,
+    ExpressRouteAdapter(makeLoadSurveyResultController())
   );
 };
