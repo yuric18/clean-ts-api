24f76c0c29f54700a796446e89d027e698fbe14e
Yuri Cabral <y.cabral18@hotmail.com>
2022-11-26 01:10:12 -0300

    refactor: starts to handle error on graphql

--- src/main/adapters/ApolloServerResolverAdapter.ts
+++ src/main/adapters/ApolloServerResolverAdapter.ts
@@ -1,9 +1,28 @@
 import { Controller } from '@/presentation';
+import {
+  ApolloError,
+  AuthenticationError,
+  ForbiddenError,
+  UserInputError,
+} from 'apollo-server-express';
 
 export const ApolloServerResolverAdapter = async (
   controller: Controller,
   args: any
 ) => {
-  const httpResponse = await controller.handle(args);
-  return httpResponse.body;
+  const response = await controller.handle(args);
+  switch (response.statusCode) {
+    case 200:
+      return response.body;
+    case 204:
+      return response.body;
+    case 400:
+      throw new UserInputError(response.body.message);
+    case 401:
+      throw new AuthenticationError(response.body.message);
+    case 403:
+      throw new ForbiddenError(response.body.message);
+    default:
+      throw new ApolloError(response.body.message);
+  }
 };
--- src/main/config/graphql.ts
+++ src/main/config/graphql.ts
@@ -3,11 +3,35 @@ import { Express } from 'express';
 
 import resolvers from '@/main/graphql/resolvers';
 import typeDefs from '@/main/graphql/typedefs';
+import { GraphQLError } from 'graphql';
+
+const handleErrors = (response, errors: GraphQLError[]) => {
+  if (
+    errors.some(
+      (e) =>
+        e.name === 'AuthenticationError' ||
+        e.originalError.name === 'AuthenticationError'
+    )
+  ) {
+    response.data = undefined;
+    response.http.status = 401;
+  }
+};
+
+const checkError = (error: GraphQLError, errorName: string): boolean =>
+  error.name === errorName || error.originalError.name === errorName;
 
 export default async (app: Express): Promise<void> => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [
+      {
+        requestDidStart: () => ({
+          willSendResponse: ({ response, errors }) => {},
+        }),
+      },
+    ],
   });
   await server.start();
   server.applyMiddleware({ app });
